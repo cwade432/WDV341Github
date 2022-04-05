fa36051f96bceedb0e645a51cc4106b61779e8c7
cwade432 <caleb.wade432@gmail.com>
2022-04-05 18:00:41 -0500

    Adding Select Events and Select One Event

---
+++ connectPDO/selectOneEvent.php
@@ -0,0 +1,77 @@
+<?php
+
+    //1. connect to db
+    //2. create sql command
+    //3. prepare statement
+    //4. bind any variables (dont have to have variables)
+    //5. execute your statement
+    // process results
+
+    $eventId = 1;
+    
+    //include 'connectPDO.php'; // brings in external file (step 1)
+    include 'connectPDOIPATCHEZ.php'; // brings in external file (step 1) // need to change based off local host or hosting account
+
+    $sql = "SELECT event_name,event_description, event_presenter FROM wdv341_events WHERE event_id=:eventId"; // (step 2)
+
+    $stmt = $conn->prepare($sql); // (step 3)
+
+    $stmt->bindParam(':eventId', $eventId); // binding variable (step $)
+
+    $stmt->execute(); // (step 5)
+
+    $stmt->setFetchMode(PDO::FETCH_ASSOC); // turn from database fromat to readable associative array (name, value)
+
+    // while($row=$stmt->fetch()) {
+    //     echo "<br> " . $row['event_name'];
+    //     echo "<br>";
+    // }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Document</title>
+
+    <style>
+        
+        span{
+            margin-right: 10px;
+        }
+
+        table, th, td {
+            border:1px solid black;
+        }
+
+    </style>
+
+</head>
+
+<body>
+    <h1>WDV 341 Intro to PHP</h1>
+    <h2>Select ONE event from table</h2>
+    <h3>Current Events!</h3>
+    <?php
+
+echo "<table style='width:100%'>";
+echo "<tr>";
+echo"<th>Event</th>
+     <th>Event Description</th>
+     <th>Event Presenter</th>";
+echo "</tr>";
+
+while($row=$stmt->fetch()) {
+    echo "<tr>";
+    echo "<td>" . $row['event_name'] . "</td>";
+    echo "<td>" . $row['event_description'] . "</td>";
+    echo "<td>" . $row['event_presenter'] . "</td>";
+    echo "</tr>";
+}
+
+    ?>
+</body>
+</html>
